1644b4cea4badfe40ddd68ba46068ddc1b50c19a
Matthew G <matt.gaba@gmail.com>
2018-01-29 16:11:26 -0500

    verify.js talking to server

--- verify.js
+++ verify.js
@@ -3,10 +3,10 @@ This basic RFID example listens for an RFID
 device to come within range of the module,
 then logs its UID to the console.
 *********************************************/
-const https = require('https');
+const http = require('http');
 var tessel = require('tessel');
 var rfidlib = require('rfid-pn532');
-const ipAdress = '172.16.22.214:3000'
+const ipAdress = 'http://172.16.22.214:3000'
 
 var rfid = rfidlib.use(tessel.port['A']);
 
@@ -14,9 +14,10 @@ rfid.on('ready', function (version) {
   console.log('Ready to read RFID card');
 
   rfid.on('data', function(card) {
-    let currentCard = '0207be03' //card.uid.toString('hex');
-    https.get(ipAdress + '/' + currentCard, res => {
-        console.log(res);
+    //let currentCard = '0207be03'
+    let currentCard = card.uid.toString('hex');
+    http.get(ipAdress + '/' + currentCard, res => {
+        console.log(res.statusCode);
     })
 
   });
